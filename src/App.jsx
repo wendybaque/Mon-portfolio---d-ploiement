4757b56acc1a0d86063242842c048976e0f04478
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-13 17:55:40 +0200

    Remove toggle and top button

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,11 @@ import Skills from "./components/Skills";
 import ProjectList from "./components/ProjectList";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-// import Toggle from "./components/Toggle";
 import "./App.css";
 
 function Home() {
   return (
     <div className="App">
-      {/* <Toggle /> */}
       <Intro />
       <About />
       <Skills />
