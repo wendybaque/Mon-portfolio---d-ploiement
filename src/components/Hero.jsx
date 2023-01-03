295e20a4a29057a7dd35d35c85c70da7c49b5bb9
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-01-03 16:06:12 +0100

    Add animated color animation in heo section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./hero.css";
+import AnimatedColors from "./AnimatedColor.jsx"
 
 const Hero = () => {
   return (
     <div>
       <header className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
-        <div className="p-5 text-2xl text-white bg-pink-600 bg-opacity-60 rounded-xl">
-          Welcome to my site!
+        <div className="text-white rounded-xl">
+          <AnimatedColors/>
         </div>
       </header>
     </div>
